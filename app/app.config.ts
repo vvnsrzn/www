e11de488df19660551fc5b29710fc033a85da907
Vivian <vivian.sarazin@hublo.com>
2025-11-08 11:37:49 +0100

    full upagrade nuxt4 nuxt-content3

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  cover: '/cover.jpg',
+  socials: {
+    github: 'vvnsrzn',
+    bluesky: 'vvnsrzn.bsky.social',
+  },
+  icon: {
+    aliases: {
+      'dark-mode': 'lucide-moon',
+      'light-mode': 'lucide-sun',
+    },
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
-  extends: "content-wind",
-  modules: ["@nuxthq/studio"],
-  compatibilityDate: "2025-01-25",
-});
+  modules: [
+    '@nuxt/content',
+    '@nuxt/icon',
+    '@nuxt/eslint',
+    '@nuxtjs/color-mode',
+  ],
+
+  css: ['assets/css/main.css'],
+  colorMode: { classSuffix: '' },
+
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+          theme: {
+            dark: 'github-dark',
+            default: 'github-light',
+          },
+        },
+      },
+    },
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2025-02-19',
+
+  vite: { plugins: [tailwindcss()] },
+})
