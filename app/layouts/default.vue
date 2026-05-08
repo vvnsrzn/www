d59457ceae0a204d40b319f83e0568d3d61ebfde
Vivian <vsarazin@invivo-group.com>
2026-05-08 18:46:12 +0200

    style: update font

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="px-4 py-10 m-auto sm:px-10 sm:py-12 max-w-2xl nb-card">
-    <main class="max-w-none">
-      <slot>
-        <h1 class="text-2xl">Page not found</h1>
-      </slot>
-    </main>
+  <div class="px-4 py-10 m-auto sm:px-0 sm:py-12 max-w-2xl">
+    <div class="nb-card px-4 py-6 sm:px-6 sm:py-8">
+      <main class="max-w-none">
+        <slot>
+          <h1 class="text-2xl">Page not found</h1>
+        </slot>
+      </main>
+    </div>
   </div>
 </template>
